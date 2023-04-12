bef76a52bb5c82f491fd9dc726628810f8226b0e
Felwah <Felwahrum@gmail.com>
2023-04-12 16:08:12 +0300

    Initial Commit

---
+++ brbightstart/brbightstartApp.swift
@@ -0,0 +1,17 @@
+//
+//  brbightstartApp.swift
+//  brbightstart
+//
+//  Created by Felwah Alrumaihi on 12/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct brbightstartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
